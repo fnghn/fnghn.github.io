c5f23711445376903ae794138e5f2cebd860321c
fnghn <4363595+fnghn@users.noreply.github.com>
2020-01-23 18:18:49 +0000

    woozy logo only loads on screens > 666px

--- res/index.js
+++ res/index.js
@@ -1,4 +1,6 @@
-const headertext = new Blotter.Text("finghin.com", {
+// fancy library code
+
+const headertext = new Blotter.Text('finghin.com', {
   family: "'Lack Regular', sans",
   size: 48,
   paddingLeft: 12,
@@ -14,27 +16,41 @@ let liquidBlotter = new Blotter(liquid, {
 
 let headertextElem, scrollElem = null;
 
-function calcHeaderScroll(element) {
+const calcHeaderScroll = (element) => {
   const viewportHeight = document.documentElement.clientHeight;
 
   return (element.scrollTop % viewportHeight)/viewportHeight;
 }
 
-function scrollEvent(evt, element) {
+const scrollEvent = (evt, element) => {
   let headerScrollPercent = calcHeaderScroll(element);
   liquid.uniforms.uSpeed.value = Math.log(headerScrollPercent + 1);
 }
 
 const observer = lozad();
 
+// mundane things
+
+const checkWindowSize = (next) => {
+  console.log(document.body.clientWidth < 666);
+  next(
+    document.body.clientWidth < 666
+  );
+};
+
 document.fonts.ready.then(() => {
   observer.observe();
 
-  headertextElem = document.getElementById("pageheader");
-  headertextElem.innerHTML = "";
+  headertextElem = document.getElementById('pageheader');
+  headertextElem.innerHTML = '';
+
+  window.addEventListener('resize', checkWindowSize((isTooSmall) => {
+    if (isTooSmall) headertextElem.style.display = 'none';
+    else headertextElem.style.display = 'block';
+  }), true);
   
-  scrollElem = document.getElementById("scroll");
-  scrollElem.addEventListener("scroll", function (e) {
+  scrollElem = document.getElementById('scroll');
+  scrollElem.addEventListener('scroll', function (e) {
     scrollEvent(e, scrollElem);
   });
   
